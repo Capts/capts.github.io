a4393376db5b31d72b0e4f75ebaf6d92f2654d98
Kevin <kevin.572.94@gmail.com>
2022-09-13 14:00:32 +0800

    Fix some designs

--- assets/js/emailScript.js
+++ assets/js/emailScript.js
@@ -3,6 +3,7 @@
 (function () {
     function validEmail(email) {
         var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+        
         return re.test(email);
     }
 
